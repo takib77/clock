8fac6cabb1a31dbd96e454a0bb10cc10d7ba1d4b
Leo <takib77@gmail.com>
2020-12-01 20:43:19 +0100

    CSS styling, and finished JS code

--- clock.js
+++ clock.js
@@ -1,3 +1,23 @@
-'use strict'
+'use strict';
 
-const time = newDate();
+
+(function getTime() {
+    const timeBox = document.querySelector('.time');
+    const time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    
+    hours = checkTime(hours);
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+    
+    function checkTime(value) {
+        if (value < 10) {
+            value = '0' + value
+        };
+        return value;
+    }
+    timeBox.textContent = (`${hours}:${minutes}:${seconds}`);
+    setInterval(getTime, 1000);
+})();
